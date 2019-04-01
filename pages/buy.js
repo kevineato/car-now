d01a6ecb2e1219aa6b28719d9d65ea1d49a7c8f6
kevineato <kevtwilson@gmail.com>
2019-03-31 22:08:54 -0500

    Add fake login, add form validation when buying.

--- pages/buy.js
+++ pages/buy.js
@@ -9,67 +9,102 @@ import { withRouter } from 'next/router'
 class Buy extends React.Component {
   constructor(props) {
     super(props)
-    this.displayAlert = this.displayAlert.bind(this)
+    this.listing = JSON.parse(props.router.query.listing)
+
+    this.state = { validated: false }
   }
 
-  displayAlert(e) {
-    e.preventDefault()
+  handleSubmit(e) {
+    const form = e.currentTarget
+    if (form.checkValidity() === false) {
+      e.preventDefault()
+      e.stopPropagation()
+    } else {
+      e.preventDefault()
+      e.stopPropagation()
+      alert(`You successfully purchased a ${this.listing.year} ${this.listing.make} ${this.listing.model}!`)
+      this.setState({validated: true})
+    }
+    form.classList.add('was-validated')
   }
+
 
   render() {
-    const listing = JSON.parse(this.props.router.query.listing)
-
     return (
       <Container>
-        <VehicleCard listing={listing} />
-        <Form onSubmit={this.displayAlert}>
+        <VehicleCard listing={this.listing} />
+        <Form noValidate validated={this.state.validated} className="needs-validation" onSubmit={e => this.handleSubmit(e)}>
           <Form.Row>
             <Form.Group as={Col} controlId="formGridPassword">
-              <Form.Label>Full Name</Form.Label>
-              <Form.Control placeholder="Full Name" />
+              <Form.Label for="customName">Full Name</Form.Label>
+              <Form.Control id="customName" required placeholder="Full Name" />
+              <div className="invalid-feedback">
+                Please enter your full name.
+              </div>
             </Form.Group>
 
             <Form.Group as={Col} controlId="formGridEmail">
               <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control required type="email" placeholder="Enter email" />
+            <div className="invalid-feedback">
+              Please enter a valid email.
+            </div>
             </Form.Group>
           </Form.Row>
 
           <Form.Group controlId="formGridAddress1">
             <Form.Label>Address</Form.Label>
-            <Form.Control placeholder="1234 Main St" />
+            <Form.Control required placeholder="1234 Main St" />
+            <div className="invalid-feedback">
+              Please enter a valid address.
+            </div>
           </Form.Group>
 
           <Form.Group controlId="formGridAddress2">
             <Form.Label>Address 2</Form.Label>
-          <Form.Control placeholder="Apartment, studio, or floor" />
+            <Form.Control placeholder="Apartment, studio, or floor" />
           </Form.Group>
 
           <Form.Row>
             <Form.Group as={Col} controlId="formGridCity">
               <Form.Label>City</Form.Label>
-              <Form.Control />
+              <Form.Control required />
+              <div className="invalid-feedback">
+                Please enter a valid city.
+              </div>
             </Form.Group>
 
             <Form.Group as={Col} controlId="formGridState">
               <Form.Label>State</Form.Label>
-              <Form.Control />
+              <Form.Control required />
+              <div className="invalid-feedback">
+                Please enter a valid state.
+              </div>
             </Form.Group>
 
             <Form.Group as={Col} controlId="formGridZip">
               <Form.Label>Zip</Form.Label>
-              <Form.Control />
+              <Form.Control required />
+              <div className="invalid-feedback">
+                Please enter a valid zip code.
+              </div>
             </Form.Group>
           </Form.Row>
 
           <Form.Row>
             <Form.Group as={Col} xs={10} controlId="formGridCardNumber">
               <Form.Label>Card Number</Form.Label>
-              <Form.Control />
+              <Form.Control required />
+              <div className="invalid-feedback">
+                Please enter a valid credit card number.
+              </div>
             </Form.Group>
             <Form.Group as={Col} xs={2} controlId="formGridSecurityCode">
               <Form.Label>Security Code</Form.Label>
-              <Form.Control />
+              <Form.Control required />
+              <div className="invalid-feedback">
+                Please enter a security code.
+              </div>
             </Form.Group>
           </Form.Row>
           <Button variant="success" type="submit">
--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,20 @@
-export default function Login() {
-  return (
-    <h1>Login Page</h1>
-  )
+import React from 'react'
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div className="d-flex min-vh-100 align-items-center justify-content-center">
+        <form onSubmit={() => alert("Thanks for signing in!")}>
+          <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+          <label htmlFor="inputEmail" className="sr-only">Email address</label>
+          <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autoFocus />
+          <label htmlFor="inputPassword" className="sr-only">Password</label>
+          <input type="password" id="inputPassword" className="form-control my-3" placeholder="Password" required />
+          <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        </form>
+      </div>
+    )
+  }
 }
+
+export default Login
